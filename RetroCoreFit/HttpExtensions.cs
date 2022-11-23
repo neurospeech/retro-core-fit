89264740cc10a5616d539cfaeb146a6b2853de93
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-11-23 21:31:10 +0530

    Parse error fixed

--- RetroCoreFit/HttpExtensions.cs
+++ RetroCoreFit/HttpExtensions.cs
@@ -41,8 +41,12 @@ namespace RetroCoreFit
                     var responseText = await r.Content.ReadAsStringAsync();
                     if (r.Content.Headers.ContentType?.MediaType?.Contains("json") ?? false)
                     {
-                        var token= Newtonsoft.Json.Linq.JToken.Parse(responseText);
-                        throw new ApiException(req.RequestUri.ToString(), r.StatusCode, responseText, token);
+                        try {
+                            var token= Newtonsoft.Json.Linq.JToken.Parse(responseText);
+                            throw new ApiException(req.RequestUri.ToString(), r.StatusCode, responseText, token);
+                        } catch (Exception ex) {
+                            
+                        }
                     }
                     throw new ApiException(req.RequestUri.ToString(), r.StatusCode, responseText, null);
                 }
